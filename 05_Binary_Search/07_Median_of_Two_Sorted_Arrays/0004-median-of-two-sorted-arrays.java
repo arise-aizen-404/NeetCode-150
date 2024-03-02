f7a59bb8dd8aed71c64a4408750f430ea1a0fca3
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-03-02 19:14:32 +0530

    bs problem -7

---
+++ 05_Binary_Search/07_Median_of_Two_Sorted_Arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,31 @@
+class Solution {
+     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+         
+// 		int arr[]=new int[nums1.length + nums2.length];
+
+// 		double ans=0; int index =0;
+// 		  for(int i=0;i<nums1.length;i++){
+// 		      arr[index]=nums1[i];
+// 		      index++;
+// 		  }
+
+// 		  for(int i=0;i<nums2.length;i++){
+// 		      arr[index]=nums2[i];
+// 		      index++;
+// 		  }
+
+// 		Arrays.sort(arr);
+// 		int len=arr.length;
+// 		int a=len/2;
+// 		    if(len%2!=0){
+// 				int val= (len+1)/2;
+// 				ans=arr[val-1];
+// 			}
+// 		    else{
+// 		        double val=(arr[a-1] + arr[a]);
+// 		        val=val/2;
+//                 ans=val;
+// 		    }
+//     return ans;    
+    }
+}
